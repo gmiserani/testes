da3aa9ad2d2251e190e480d0534c2fbbf4611f91
gmiserani <gabrielamiserani@uol.com.br>
2023-11-23 23:37:42 -0300

    retest

--- calc.py
+++ calc.py
@@ -3,4 +3,8 @@ class Calculadora:
     def somar(self, a, b):
         return a + b
 
-    
+    def subtrair(self, a, b):
+        return a - b
+
+    def multiplicar(self, a, b):
+        return a * b
