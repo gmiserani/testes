78f73f0fb0e82670c8310ac254206494f0c5ccff
gmiserani <gabrielamiserani@uol.com.br>
2023-11-23 23:33:07 -0300

    change to test the actions

--- calc.py
+++ calc.py
@@ -3,8 +3,4 @@ class Calculadora:
     def somar(self, a, b):
         return a + b
 
-    def subtrair(self, a, b):
-        return a - b
-
-    def multiplicar(self, a, b):
-        return a * b
+    
